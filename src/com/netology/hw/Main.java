4d7e986a54c1c5bd90ab4ede42ad69a3c3aea5ff
dianest <nddgdd@gmail.com>
2020-08-25 21:16:16 +0100

    Add src

---
+++ src/com/netology/hw/Main.java
@@ -0,0 +1,10 @@
+package com.netology.hw;
+
+public class Main {
+    public static void main(String[] args) {
+        double regularBonus = 0.3;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
